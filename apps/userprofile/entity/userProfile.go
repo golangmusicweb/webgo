e3abe2299745fa527e787165d418ad408c7bdc1e
dongxy <870428371@qq.com>
2018-07-06 14:54:45 +0800

    修改用户实体配置

--- apps/userprofile/entity/userProfile.go
+++ apps/userprofile/entity/userProfile.go
@@ -6,12 +6,12 @@ import (
 
 type UserProfile struct {
 	Id int64 `xorm: "pk autoincr notnull" json: "id"`
-	Password string `xorm: "varchar(40) notnull" json: "passwd"`
-	Email string `xorm: "varchar(40) unique notnull" json: "email"`
+	Password string `xorm: "varchar(255) notnull" json: "passwd"`
+	Email string `xorm: "varchar(255) unique notnull" json: "email"`
 	Phone int64 `xorm: "bigint" json: "phone"`
-	Role string `xorm: "varchar(40) notnull" json: "role"`
-	Nickname string `xorm: "varchar(40)" json: "nickname"`
+	Role string `xorm: "varchar(255) notnull" json: "role"`
+	Nickname string `xorm: "varchar(255)" json: "nickname"`
 	Birthday time.Time `xorm: "date" json: "birthday"`
-	Address string `xorm: "varchar(40)" json: "address"`
+	Address string `xorm: "varchar(255)" json: "address"`
 	CreatedAt time.Time `xorm: "created" json: "created_at"`
 }
